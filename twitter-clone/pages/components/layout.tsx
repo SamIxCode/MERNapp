9ab8a2eea3ee3d420247f72e521b08ed6f6a1580
Samuel Rybar <samuel.rybar@cargo-partner.com>
2023-03-29 20:38:26 +0200

    added follow button

--- twitter-clone/pages/components/layout.tsx
+++ twitter-clone/pages/components/layout.tsx
@@ -1,3 +1,4 @@
+import Followbar from "./layouts/Followbar";
 import Sidebar from "./layouts/sidebar";
 
 interface LayoutProps {
@@ -13,6 +14,7 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
                 <div className=" col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
                     {children}
                 </div>
+                <Followbar />
             </div>
         </div>
     </div>
--- twitter-clone/pages/components/layouts/sidebar.tsx
+++ twitter-clone/pages/components/layouts/sidebar.tsx
@@ -1,7 +1,9 @@
 import {BsHouseFill,BsBellFill} from 'react-icons/bs';
 import {FaUser} from 'react-icons/fa';
+import {BiLogOut} from 'react-icons/bi';
 import SidebarLogo from './SidebarLogo';
 import SidebarItem from './SidebarItem';
+import SidebarTweetButton from './SidebarTweetButton';
 
 const Sidebar = () => {
   const items =[
@@ -35,6 +37,8 @@ const Sidebar = () => {
 
             />
           ))}
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label='Log out' />
+          <SidebarTweetButton />
         </div>
       </div>
     </div>
